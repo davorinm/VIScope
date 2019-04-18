d055f99f81269aae9cf4d30cff37f7678f7e2c67
Davorin Mađarić <davorin.madaric@inova.si>
2019-04-18 10:23:46 +0200

    Added Charts

---
+++ Charts/Charts/Charts.h
@@ -0,0 +1,19 @@
+//
+//  Charts.h
+//  Charts
+//
+//  Created by Davorin Mađarić on 18/04/2019.
+//  Copyright © 2019 Davorin Mađarić. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Charts.
+FOUNDATION_EXPORT double ChartsVersionNumber;
+
+//! Project version string for Charts.
+FOUNDATION_EXPORT const unsigned char ChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Charts/PublicHeader.h>
+
+
